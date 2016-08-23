af6a3e4dd784f55e4b2135d29f16853928ce0685
Zhakel <zdk930519@sina.com>
2016-08-23 15:43:49 +0800

    a new start

---
+++ exercise/char[].cpp
@@ -0,0 +1,11 @@
+char *lines[5] = {
+        "COSC1283/1284",
+        "Programming",
+        "Techniques",
+        "is",
+        "great fun"
+    };
+	
+char c2 = (*lines + 5)[5];
+
+printf("  c2 = %c\n", c2);
