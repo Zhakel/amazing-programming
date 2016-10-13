e43c24246e77e20bd0dfa6d5e4316102f941e41f
Zhakel <zdk930519@gmail.com>
2016-10-13 11:01:38 +0800

    添加fabomacci算法和lame定理

--- exercise/Euclid_GCD.c
+++ exercise/Euclid_GCD.c
@@ -2,12 +2,19 @@
 
 //get GCD(greatest common divisor)
 int euclid(int,int);
-
+int fabonacci(int);
+int smallnum(int,int);
+//Lame (small number of euclid is greater than fabonacci(count))
+int count = 0;
 int main(void)
 {
 	int s =0;
-	s = euclid(12,36);
+	int a=14;
+	int b =26;
+	s = euclid(a,b);
 	printf("%d\n", s);
+	printf("smallnum is:%d\n", smallnum(a,b));
+	printf("fabonacci is :%d\n", fabonacci(count));
 	return 0;
 }
 
@@ -20,9 +27,29 @@ int euclid(int a,int b)
 		return euclid(b,a);
 	}
 	if(a%b==0)
+	{
+		count++;
 		return b;
+	}
 	else
 	{
+		count++;
 		return euclid(b,a%b);
 	}
+}
+
+int fabonacci(int a)
+{
+	if(a==1||a==0)
+		return 1;
+	else
+		return fabonacci(a-1)+fabonacci(a-2);
+}
+
+int smallnum(int a,int b)
+{
+	if(a>=b)
+		return b;
+	else
+		return a;
 }
